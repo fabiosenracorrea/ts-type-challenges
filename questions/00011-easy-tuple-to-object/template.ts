d62c15184509e80d59b1609957ec662659ecf975
Fabio Correa <fabiosenracorrea@hotmail.com>
2025-02-28 13:56:34 -0300

    11 complement

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -3,3 +3,9 @@ type ObjKey = string | number | symbol
 type TupleToObject<T extends readonly ObjKey[]> = {
   [K in T[number]]: K
 }
+
+// Fun fact: We have a built-in for that: PropertyKey
+
+type TupleToObject2<T extends readonly PropertyKey[]> = {
+  [K in T[number]]: K
+}
