3041304f5daacafd33476a4948ac28bd0d4519b5
Fabio Correa <fabiosenracorrea@hotmail.com>
2025-02-28 16:54:04 -0300

    ts omit

--- questions/00003-medium-omit/test-cases.ts
+++ questions/00003-medium-omit/test-cases.ts
@@ -39,3 +39,8 @@ interface Expected3 {
 type MyOmit<T, K extends keyof T> = {
   [Key in keyof T as Key extends K ? never : Key]: T[Key]
 }
+
+// Omit<> actually does not constrict like this exercise asks for
+type TsOmit<T, K> = {
+  [Key in keyof T as Key extends K ? never : Key]: T[Key]
+}
