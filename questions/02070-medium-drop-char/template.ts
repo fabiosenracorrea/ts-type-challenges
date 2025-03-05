b7e03f00ec72fe8dc79cecea474a35a10cbbfce4
Fabio Correa <fabiosenracorrea@hotmail.com>
2025-03-05 16:55:48 -0300

    dropchar2

--- questions/02070-medium-drop-char/template.ts
+++ questions/02070-medium-drop-char/template.ts
@@ -4,3 +4,13 @@ type DropChar<T extends string, Char extends string, Acc extends string = ''> =
       ? DropChar<Rest, Char, Acc>
       : DropChar<Rest, Char, `${Acc}${P}`>
     : Acc
+
+/**
+ * Solution 2: No Accumulator
+ *
+ * youtube.com/watch?v=-z4jxgdKrag
+ */
+type DropChar2<T extends string, Char extends string> =
+  T extends `${infer Left}${Char}${infer Right}`
+    ? DropChar2<`${Left}${Right}`, Char>
+    : T
--- questions/02070-medium-drop-char/test-cases.ts
+++ questions/02070-medium-drop-char/test-cases.ts
@@ -13,7 +13,7 @@ type cases = [
 
 // ------------------- IMPLEMENTATION --------------------------- //
 
-type DropChar<T extends string, Char extends string, Acc extends string = ''> =
+type DropChar2<T extends string, Char extends string, Acc extends string = ''> =
   T extends `${infer P}${infer Rest}`
     ? P extends Char
       ? DropChar<Rest, Char, Acc>
