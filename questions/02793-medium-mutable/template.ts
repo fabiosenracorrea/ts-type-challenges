77c666196298dfe8f08fd4b7456005236b98467e
Fabio Correa <fabiosenracorrea@hotmail.com>
2025-03-05 19:52:28 -0300

    02793 - Mutable Obj

--- questions/02793-medium-mutable/template.ts
+++ questions/02793-medium-mutable/template.ts
@@ -1,1 +1,14 @@
-type Mutable<T> = any
+// Otherwise the readonly is still there
+type GetKeys<T> = { [K in keyof T]: K }[keyof T]
+
+type GetItems<T extends readonly any[]> =
+  T extends readonly [infer P, ...infer Rest]
+    ? [P, ...Rest]
+    : never
+
+type Mutable<T extends Record<string, unknown> | readonly any[]> =
+  T extends Record<string, unknown>
+    ? { [K in GetKeys<T>]: T[K] }
+    : T extends readonly any[]
+      ? GetItems<T>
+      : never
--- questions/02793-medium-mutable/test-cases.ts
+++ questions/02793-medium-mutable/test-cases.ts
@@ -22,3 +22,20 @@ type errors = [
   // @ts-expect-error
   Mutable<0>,
 ]
+
+// ------------------- IMPLEMENTATION --------------------------- //
+
+// Otherwise the readonly is still there
+type GetKeys<T> = { [K in keyof T]: K }[keyof T]
+
+type GetItems<T extends readonly any[]> =
+  T extends readonly [infer P, ...infer Rest]
+    ? [P, ...Rest]
+    : never
+
+type Mutable<T extends Record<string, unknown> | readonly any[]> =
+  T extends Record<string, unknown>
+    ? { [K in GetKeys<T>]: T[K] }
+    : T extends readonly any[]
+      ? GetItems<T>
+      : never
