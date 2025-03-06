04f15ba61204c10285f149f0508f7b11aa916ece
Fabio Correa <fabiosenracorrea@hotmail.com>
2025-03-06 19:40:55 -0300

    5153 - IndexOf

--- questions/05153-medium-indexof/template.ts
+++ questions/05153-medium-indexof/template.ts
@@ -1,1 +1,10 @@
-type IndexOf<T, U> = any
+import type { Equal } from '@type-challenges/utils'
+
+// ------------------- IMPLEMENTATION --------------------------- //
+
+type IndexOf<List extends unknown[], Target, Acc extends readonly unknown[] = []>
+  = List extends [infer First, ...infer Rest]
+    ? Equal<First, Target> extends true
+      ? Acc['length']
+      : IndexOf<Rest, Target, [...Acc, 1]>
+    : -1
--- questions/05153-medium-indexof/test-cases.ts
+++ questions/05153-medium-indexof/test-cases.ts
@@ -9,3 +9,12 @@ type cases = [
   Expect<Equal<IndexOf<[string, 'a'], 'a'>, 1>>,
   Expect<Equal<IndexOf<[any, 1], 1>, 1>>,
 ]
+
+// ------------------- IMPLEMENTATION --------------------------- //
+
+type IndexOf<List extends unknown[], Target, Acc extends readonly unknown[] = []>
+  = List extends [infer First, ...infer Rest]
+    ? Equal<First, Target> extends true
+      ? Acc['length']
+      : IndexOf<Rest, Target, [...Acc, 1]>
+    : -1
