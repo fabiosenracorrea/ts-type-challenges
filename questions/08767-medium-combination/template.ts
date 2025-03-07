62e8921e641fc7e084d4de66a04136a678849a57
Fabio Correa <fabiosenracorrea@hotmail.com>
2025-03-06 23:46:08 -0300

    Combination2

--- questions/08767-medium-combination/template.ts
+++ questions/08767-medium-combination/template.ts
@@ -10,3 +10,16 @@ type Combination<T extends readonly string[]> =
   Trim<
     Exclude<CombinationInner<T>, ''>
   >
+
+// Note: because of the distributive property x hack we use
+// if we do not declare U = T[number] and pass in T[number] to
+// the exclude, it does not work as intended
+// youtube.com/watch?v=BgbZKd2DpQM
+type Combination2<
+  T extends string[],
+  Union = T[number],
+  Acc = Union,
+> =
+  Acc extends string
+    ? Acc | `${Acc} ${Combination2<[], Exclude<Union, Acc>>}`
+    : never
--- questions/08767-medium-combination/test-cases.ts
+++ questions/08767-medium-combination/test-cases.ts
@@ -34,3 +34,37 @@ type Combination<T extends readonly string[]> =
   Trim<
     Exclude<CombinationInner<T>, ''>
   >
+
+// ------------------- USING A SIMILAR LOGIC TO PERMUTATION --------------------------- //
+
+type cases2 = [
+  Expect<Equal<Combination2<['foo', 'bar', 'baz']>, 'foo' | 'bar' | 'baz' | 'foo bar' | 'foo bar baz' | 'foo baz' | 'foo baz bar' | 'bar foo' | 'bar foo baz' | 'bar baz' | 'bar baz foo' | 'baz foo' | 'baz foo bar' | 'baz bar' | 'baz bar foo'>>,
+  Expect<Equal<Combination2<['apple', 'banana', 'cherry']>, 'apple' | 'banana' | 'cherry' |
+  'apple banana' | 'apple cherry' | 'banana apple' | 'banana cherry' | 'cherry apple' | 'cherry banana' |
+  'apple banana cherry' | 'apple cherry banana' | 'banana apple cherry' | 'banana cherry apple' | 'cherry apple banana' | 'cherry banana apple'>>,
+  Expect<Equal<Combination2<['red', 'green', 'blue', 'yellow']>, 'red' | 'green' | 'blue' | 'yellow' |
+  'red green' | 'red blue' | 'red yellow' | 'green red' | 'green blue' | 'green yellow' | 'blue red' | 'blue green' | 'blue yellow' | 'yellow red' | 'yellow green' | 'yellow blue' |
+  'red green blue' | 'red green yellow' | 'red blue green' | 'red blue yellow' | 'red yellow green' | 'red yellow blue' |
+  'green red blue' | 'green red yellow' | 'green blue red' | 'green blue yellow' | 'green yellow red' | 'green yellow blue' |
+  'blue red green' | 'blue red yellow' | 'blue green red' | 'blue green yellow' | 'blue yellow red' | 'blue yellow green' |
+  'yellow red green' | 'yellow red blue' | 'yellow green red' | 'yellow green blue' | 'yellow blue red' | 'yellow blue green' |
+  'red green blue yellow' | 'red green yellow blue' | 'red blue green yellow' | 'red blue yellow green' | 'red yellow green blue' | 'red yellow blue green' |
+  'green red blue yellow' | 'green red yellow blue' | 'green blue red yellow' | 'green blue yellow red' | 'green yellow red blue' | 'green yellow blue red' |
+  'blue red green yellow' | 'blue red yellow green' | 'blue green red yellow' | 'blue green yellow red' | 'blue yellow red green' | 'blue yellow green red' |
+  'yellow red green blue' | 'yellow red blue green' | 'yellow green red blue' | 'yellow green blue red' | 'yellow blue red green' | 'yellow blue green red'>>
+  ,
+  Expect<Equal<Combination2<['one', 'two']>, 'one' | 'two' |
+  'one two' | 'two one'>>,
+]
+
+// Note: because of the distributive property x hack we use
+// if we do not declare U = T[number] and pass in T[number] to
+// the exclude, it does not work as intended
+type Combination2<
+  T extends string[],
+  Union = T[number],
+  Acc = Union,
+> =
+    Acc extends string
+      ? Acc | `${Acc} ${Combination2<[], Exclude<Union, Acc>>}`
+      : never
