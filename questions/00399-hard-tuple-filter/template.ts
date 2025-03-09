30851707c9612b303faf96160692e00adc8d2c42
Fabio Correa <fabiosenracorrea@hotmail.com>
2025-03-09 12:45:18 -0300

    00399 - Tuple Filter

--- questions/00399-hard-tuple-filter/template.ts
+++ questions/00399-hard-tuple-filter/template.ts
@@ -1,1 +1,6 @@
-type FilterOut<T extends any[], F> = any
+type FilterOut<List extends readonly unknown[], RemoveType> =
+  List extends [infer Next, ...infer Rest]
+    ? [Next] extends [RemoveType] // compare using the never check
+        ? FilterOut<Rest, RemoveType>
+        : [Next, ...FilterOut<Rest, RemoveType>]
+    : []
--- questions/00399-hard-tuple-filter/test-cases.ts
+++ questions/00399-hard-tuple-filter/test-cases.ts
@@ -8,3 +8,12 @@ type cases = [
   Expect<Equal<FilterOut<[never, 1, 'a', undefined, false, null], never | null | undefined>, [1, 'a', false]>>,
   Expect<Equal<FilterOut<[number | null | undefined, never], never | null | undefined>, [number | null | undefined]>>,
 ]
+
+// ------------------- IMPLEMENTATION --------------------------- //
+
+type FilterOut<List extends readonly unknown[], RemoveType> =
+  List extends [infer Next, ...infer Rest]
+    ? [Next] extends [RemoveType] // compare using the never check
+        ? FilterOut<Rest, RemoveType>
+        : [Next, ...FilterOut<Rest, RemoveType>]
+    : []
