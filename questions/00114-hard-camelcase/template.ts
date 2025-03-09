520852bbbbec174fe6650e40c209ee9a0e8193bd
Fabio Correa <fabiosenracorrea@hotmail.com>
2025-03-09 09:06:57 -0300

    00114 - Cammelcase

--- questions/00114-hard-camelcase/template.ts
+++ questions/00114-hard-camelcase/template.ts
@@ -1,1 +1,33 @@
-type CamelCase<S extends string> = any
+type IsLetter_<T extends string> = Uppercase<T> extends Lowercase<T> ? false : true
+
+/**
+ * Removes the '_' only if the following char is a letter
+ */
+type ShouldRemoveChar<Char extends string, RemainingChars extends string> =
+  Char extends '_'
+    ? RemainingChars extends `${infer Next}${string}`
+      ? IsLetter_<Next>
+      : false
+    : false
+
+type ParseNext<
+  Char extends string,
+  ShouldUpper extends boolean,
+  RemainingChars extends string,
+> =
+  ShouldRemoveChar<Char, RemainingChars> extends true
+    ? ''
+    : ShouldUpper extends true ? Uppercase<Char> : Lowercase<Char>
+
+type CamelCase<
+  T extends string,
+  Transform extends boolean = false,
+> =
+  T extends `${infer NextChar}${infer Rest}`
+    ? `${
+      ParseNext<NextChar, Transform, Rest>
+    }${
+      CamelCase<Rest, NextChar extends '_' ? true : false>
+    }`
+
+    : Lowercase<T>
--- questions/00114-hard-camelcase/test-cases.ts
+++ questions/00114-hard-camelcase/test-cases.ts
@@ -15,3 +15,39 @@ type cases = [
   Expect<Equal<CamelCase<''>, ''>>,
   Expect<Equal<CamelCase<'😎'>, '😎'>>,
 ]
+
+// ------------------- IMPLEMENTATION --------------------------- //
+
+type IsLetter<T extends string> = Uppercase<T> extends Lowercase<T> ? false : true
+
+/**
+ * Removes the '_' only if the following char is a letter
+ */
+type ShouldRemoveChar<Char extends string, RemainingChars extends string> =
+  Char extends '_'
+    ? RemainingChars extends `${infer Next}${string}`
+      ? IsLetter<Next>
+      : false
+    : false
+
+type ParseNext<
+  Char extends string,
+  ShouldUpper extends boolean,
+  RemainingChars extends string,
+> =
+  ShouldRemoveChar<Char, RemainingChars> extends true
+    ? ''
+    : ShouldUpper extends true ? Uppercase<Char> : Lowercase<Char>
+
+type CamelCase<
+  T extends string,
+  Transform extends boolean = false,
+> =
+  T extends `${infer NextChar}${infer Rest}`
+    ? `${
+      ParseNext<NextChar, Transform, Rest>
+    }${
+      CamelCase<Rest, NextChar extends '_' ? true : false>
+    }`
+
+    : Lowercase<T>
