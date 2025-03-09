c8f6f5e1e7b61f4dfe9da4042b545ff29ea95b5c
Fabio Correa <fabiosenracorrea@hotmail.com>
2025-03-09 15:52:42 -0300

    00730 - Union to Tuple

--- README.md
+++ README.md
@@ -64,6 +64,7 @@ Last synced: 2025-02-28
 - [00612 - Kebab Case](./questions/00612-medium-kebabcase)
 - [00645 - Obj Diff](./questions/00645-medium-diff)
 - [00651 - Length of string 2](./questions/00651-hard-length-of-string-2)
+- [00730 - Union to Tuple](./questions/00730-hard-union-to-tuple)
 - [00898 - Includes](./questions/00898-easy-includes)
 - [00949 - AnyOf](./questions/00949-medium-anyof)
 - [01042 - IsNever](./questions/01042-medium-isnever)
--- questions/00730-hard-union-to-tuple/test-cases.ts
+++ questions/00730-hard-union-to-tuple/test-cases.ts
@@ -15,3 +15,49 @@ type cases = [
   Expect<Equal<ExtractValuesOfTuple<UnionToTuple<never>>, never>>,
   Expect<Equal<ExtractValuesOfTuple<UnionToTuple<'a' | 'b' | 'c' | 1 | 2 | 'd' | 'e' | 'f' | 'g'>>, 'f' | 'e' | 1 | 2 | 'g' | 'c' | 'd' | 'a' | 'b'>>,
 ]
+
+// ------------------- IMPLEMENTATION --------------------------- //
+
+type UnionToIntersection_<T> =
+  (
+    T extends T ? (arg: T) => 0 : never
+  ) extends (param: infer P) => 0
+    ? P
+    : never
+
+/**
+ *  This works because of function overloads in TS
+ *  If its necessary to output the fn arguments,
+ *  ts will just output the last one
+ *
+ * 1. Basically we have any union a | b
+ *
+ * 2. we convert that to a function union, distributively: (a) => void | (b) => void <=== This is what happens inside the UnionToIntersection_<__HERE__> below
+ *   **(We use the T extends T to force TS to apply the type to each member of the union instead of creating (a|b) => void
+ *
+ * 3. UnionToIntersection does its magic, converting:
+ *      FROM: (a) => void | (b) => void
+ *      TO:   (a) => void & (b) => void
+ *
+ * 4. Because of typescript behavior, it outputs the last arg if necessary `b`
+ *
+ * We can use this to both 1. add it to the tuple + remove from the original union and walk in it until `never` is achieved
+ *
+ * youtube.com/watch?v=hphL3xRFm_s
+ */
+type LastInUnion<T> =
+  UnionToIntersection_<
+    T extends T ? (arg: T) => 0 : never
+  > extends (param: infer P) => 0
+    ? P
+    : never
+
+type UnionToTuple<
+  T,
+  Last = LastInUnion<T>,
+> = [Last] extends [never]
+  ? []
+  : [...UnionToTuple<Exclude<T, Last>>, Last]
+
+type xxx = UnionToTuple<'a' | 'b'>
+type yyy = UnionToTuple<'a' | 'b' | 'c' | 1 | 2 | 'd' | 'e' | 'f' | 'g'>
